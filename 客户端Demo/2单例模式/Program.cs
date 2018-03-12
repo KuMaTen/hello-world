830ccd6873acef8e94eca5f3dffec62ddd283e6c
zuocigui <Zuocg@ai-titan.com>
2018-03-12 16:12:00 +0800

    '修改一行代码 2018-3-12'

--- 客户端Demo/2单例模式/Program.cs
+++ 客户端Demo/2单例模式/Program.cs
@@ -17,6 +17,7 @@ namespace _2单例模式
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Hello!!");
         }
     }
 }
